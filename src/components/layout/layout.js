af46dc3bfb1fae9820a7458b4c8cfecd0e59503a
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-02-24 15:32:48 -0600

    Remove typekit setup

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./layout.css"
+import { Helmet } from "react-helmet";
 import Header from "../header/header"
 import Footer from "../footer/footer"
 import MegaMenu from "../mega-menu/mega-menu"
@@ -7,6 +8,9 @@ import MegaMenu from "../mega-menu/mega-menu"
 const Layout = props => {
   return (
     <div>
+      <Helmet>
+        <link href="//cloud.typenetwork.com/projects/5316/fontface.css/" rel="stylesheet" type="text/css"/>
+      </Helmet>
       <Header />
       <MegaMenu />
       {props.children}

498b50fe76e299e71cc90ec4176ba2a91b8b3b7b
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-03-02 16:23:55 -0600

    Fix fullscreen issue on videos

--- src/components/content-block/content-block.js
+++ src/components/content-block/content-block.js
@@ -109,7 +109,7 @@ const ContentBlock = (props) => {
                     <span></span>
                     <span></span>
                   </button>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/kcyUY10D3N4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/kcyUY10D3N4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </Modal>                
               </div>
             </div> 

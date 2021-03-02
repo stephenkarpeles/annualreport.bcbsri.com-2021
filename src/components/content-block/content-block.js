2273c1942536c8c7933dc7b78037caa6a7ac36c6
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-03-02 14:28:17 -0600

    Add blockquote module, and mobile sizing

--- src/components/content-block/content-block.js
+++ src/components/content-block/content-block.js
@@ -17,8 +17,20 @@ const ContentBlock = (props) => {
             <div className="content-block__col content-block__col--left">
               <p>In 2020, we provided multiple waves of medical and dental premium relief to our customers. Those included fully insured large group and small group customers as well as individuals who purchase insurance through BCBSRI or HealthSource RI.</p>
               
-              <div className="mar-top-2">
+              <div className="content-block__image">
                 <img src={piggyBankImg} alt=""/>
+              </div>
+
+              <div className="content-block__blockquote">
+                <div className="content-block__blockquote-text">
+                  “When Rhode Island PBS launches a project of interest and importance to the health and well-being of our community—whether it is prevention of opioid overdose or now COVID-19 information—we are grateful we can count on Blue Cross’s leadership and enthusiastic partnership.”
+                </div>
+                <div className="content-block__blockquote-author">
+                  David W. Piccerelli
+                </div>
+                <div className="content-block__blockquote-author-title">
+                  President of WSBE Rhode Island PBS
+                </div>
               </div>
             </div>
             <div className="content-block__col content-block__col--right">

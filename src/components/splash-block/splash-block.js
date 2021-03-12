c795066cea8d316bd38af063911cdd58d7a5c51f
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-03-12 08:07:27 -0600

    Splash intro

---
+++ src/components/splash-block/splash-block.js
@@ -0,0 +1,45 @@
+
+import * as React from "react"
+import Fade from 'react-reveal/Fade'
+import Slide from 'react-reveal/Slide'
+
+import "./splash-block.css"
+import heroImg1 from "../../images/hero-image-1.jpg"
+import heroImg2 from "../../images/hero-image-2.jpg"
+import heroImg3 from "../../images/hero-image-3.jpg"
+import heroImg4 from "../../images/hero-image-4.jpg"
+
+const SplashBlock = (props) => {
+  return (
+    <div className={`splash-block ${props.styleName}`}>
+      <div className="container container--small">
+        <div className="splash-block__title">
+          <Fade>
+            <h1>
+              <span>2020</span><br/>
+              Annual and<br/>Community Report
+            </h1>
+          </Fade>
+        </div>
+      </div>
+      <Slide bottom cascade>
+        <div className="splash-block__image-row">        
+          <div className="splash-block__image">
+            <img src={heroImg1} alt=""/>
+          </div>
+          <div className="splash-block__image">
+            <img src={heroImg2} alt=""/>
+          </div>
+          <div className="splash-block__image">
+            <img src={heroImg3} alt=""/>
+          </div>
+          <div className="splash-block__image">
+            <img src={heroImg4} alt=""/>
+          </div>
+        </div>
+      </Slide>      
+    </div>
+  )
+}
+
+export default SplashBlock
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import * as React from "react"
 import Fade from 'react-reveal/Fade'
-import Slide from 'react-reveal/Slide'
+// import Slide from 'react-reveal/Slide'
 
 // Components
 import Layout from "../components/layout/layout"
 import ByTheNumbers from "../components/by-the-numbers/by-the-numbers"
 import NumberBlock from "../components/number-block/number-block"
+import SplashBlock from "../components/splash-block/splash-block"
 
 const IndexPage = () => {
   return (
     <Layout>
+          
+      <SplashBlock/>
 
       <section className="content-block" style={{ backgroundColor: "#fff"}}>
         <Fade>
@@ -20,11 +23,14 @@ const IndexPage = () => {
             </div>
             <div className="content-block__col-wrap">
               <div className="content-block__col content-block__col--left">
-                <p>There are no adequate words to describe the shock to the system that was 2020. A public health crisis led to an economic crisis. Heightened social unrest sprang from a backlash against longstanding racial injustice in our country. The crises of 2020 did not define Blue Cross & Blue Shield of Rhode Island as a company, however. Rather, we regarded them as an accelerating force for change and a call to action. While our annual and community report cannot fully capture the impact of these monumental events on Rhode Islanders, what you will find here is cause for hope in a portrait of strength, resilience, and caring. We saw the pandemic bring out the best in Rhode Islanders. Front line workers – postal carriers, grocery clerks, pharmacists, delivery drivers, nursing home staff, directors of homeless shelters – and all healthcare professionals – were elevated to hero status, putting their lives on the line. Blue Cross employees were inspired. They volunteered their time to sew masks and deliver meals with Meals on Wheels. They worked with the Rhode Island Coalition of the Homeless to get necessary supplies to dozens of homeless shelters and service providers across the state to keep people safe and mitigate community spread. Our company provided $25 million in premium relief to members and customers. We served as advisors to the Department of Health and collected and shared relevant data. We waived cost sharing for telehealth visits, and for testing, visits, and care related to COVID-19. At the end of the year, we delivered groceries to our most vulnerable Medicaid members.</p>         
+                <p>There are no adequate words to describe the shock to the system that was 2020. A public health crisis led to an economic crisis. Heightened social unrest sprang from a backlash against longstanding racial injustice in our country. The crises of 2020 did not define Blue Cross & Blue Shield of Rhode Island as a company, however. Rather, we regarded them as an accelerating force for change and a call to action. While our annual and community report cannot fully capture the impact of these monumental events on Rhode Islanders, what you will find here is cause for hope in a portrait of strength, resilience, and caring.</p>
+                <p>We saw the pandemic bring out the best in Rhode Islanders. Front line workers – postal carriers, grocery clerks, pharmacists, delivery drivers, nursing home staff, directors of homeless shelters – and all healthcare professionals – were elevated to hero status, putting their lives on the line.</p>
+                <p>Blue Cross employees were inspired. They volunteered their time to sew masks and deliver meals with Meals on Wheels. They worked with the Rhode Island Coalition of the Homeless to get necessary supplies to dozens of homeless shelters and service providers across the state to keep people safe and mitigate community spread. Our company provided $25 million in premium relief to members and customers. We served as advisors to the Department of Health and collected and shared relevant data. We waived cost sharing for telehealth visits, and for testing, visits, and care related to COVID-19. At the end of the year, we delivered groceries to our most vulnerable Medicaid members.</p>         
 
               </div>
               <div className="content-block__col content-block__col--right">
-                <p>And we reinforced our commitment to comprehensive health and well-being for all Rhode Islanders. Well before the pandemic and economic crisis that followed in its wake starkly underscored the inequities in our state, we had teamed up with the Brown University School of Public Health to address social factors influencing health outcomes, like poverty and structural racism. Armed with data from our RI Life Index confirming that lack of safe and affordable housing was a barrier to Rhode Islanders’ health and well-being, we made significant grants to organizations addressing this issue. We treasure the trust you have placed in us. Going forward, we pledge to continue earning that trust. In the past year, we have learned valuable lessons about what it means to humbly listen, lead, and serve – as a resource, as a partner, and as a force for positive change.</p>
+                <p>And we reinforced our commitment to comprehensive health and well-being for all Rhode Islanders. Well before the pandemic and economic crisis that followed in its wake starkly underscored the inequities in our state, we had teamed up with the Brown University School of Public Health to address social factors influencing health outcomes, like poverty and structural racism. Armed with data from our RI Life Index confirming that lack of safe and affordable housing was a barrier to Rhode Islanders’ health and well-being, we made significant grants to organizations addressing this issue.</p>
+                <p>We treasure the trust you have placed in us. Going forward, we pledge to continue earning that trust. In the past year, we have learned valuable lessons about what it means to humbly listen, lead, and serve – as a resource, as a partner, and as a force for positive change.</p>
               </div> 
             </div>     
           </div>
@@ -33,31 +39,31 @@ const IndexPage = () => {
 
       <Fade>
         <div className="flex-block">
-        <NumberBlock
-          numBlockNumber="200"
-          numBlockCaption="organizations supported through in-kind donations, volunteerism, or financial support"
-        />
-        <NumberBlock
-          numBlockNumber="56,646"
-          numBlockCaption="people impacted by charitable funding or volunteerism"
-        />
-        <NumberBlock
-          numBlockNumber="6,500"
-          numBlockCaption="volunteer hours served"
-        />
-        <NumberBlock
-          numBlockNumber={[<><sup>$</sup>176,800</>]}
-          numBlockCaption={[<>value of volunteer hours<sup>1</sup></>]}
-        />
-        <NumberBlock
-          numBlockNumber={[<><sup>$</sup>142,345</>]}
-          numBlockCaption="generated by employee fundraising"
-        />
-        <NumberBlock
-          numBlockNumber={[<><sup>$</sup>3,207,818</>]}
-          numBlockCaption="invested by the company through financial or in-kind donations or distributions from the Rhode Island Community Health Fund at the RI Foundation."
-        />
-      </div>
+          <NumberBlock
+            numBlockNumber="200"
+            numBlockCaption="organizations supported through in-kind donations, volunteerism, or financial support"
+          />
+          <NumberBlock
+            numBlockNumber="56,646"
+            numBlockCaption="people impacted by charitable funding or volunteerism"
+          />
+          <NumberBlock
+            numBlockNumber="6,500"
+            numBlockCaption="volunteer hours served"
+          />
+          <NumberBlock
+            numBlockNumber={[<><sup>$</sup>176,800</>]}
+            numBlockCaption={[<>value of volunteer hours<sup>1</sup></>]}
+          />
+          <NumberBlock
+            numBlockNumber={[<><sup>$</sup>142,345</>]}
+            numBlockCaption="generated by employee fundraising"
+          />
+          <NumberBlock
+            numBlockNumber={[<><sup>$</sup>3,207,818</>]}
+            numBlockCaption="invested by the company through financial or in-kind donations or distributions from the Rhode Island Community Health Fund at the RI Foundation."
+          />
+        </div>
       </Fade>
       <ByTheNumbers/>
     </Layout>

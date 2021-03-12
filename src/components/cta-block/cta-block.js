d336d4e6dcd928bea7a8ce474bb00ff6697a8c34
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-03-12 10:12:08 -0600

    Home angle

--- src/components/cta-block/cta-block.js
+++ src/components/cta-block/cta-block.js
@@ -5,17 +5,23 @@ import Fade from 'react-reveal/Fade'
 
 import "./cta-block.css"
 
-const CtaBlock = (props) => {
+const CtaBlock = () => {
   return (
     <div className="cta-block">
       <div className="cta-block__item cta-block__item--health-well-being">
-        <h3>Health &amp;<br/>Well-Being</h3>
+        <Fade>
+          <h3>Health &amp;<br/>Well-Being</h3>
+        </Fade>
       </div>
       <div className="cta-block__item cta-block__item--customer-convenience">
-        <h3>Customer<br/>Convenience</h3>
+        <Fade>
+          <h3>Customer<br/>Convenience</h3>
+        </Fade>
       </div>
       <div className="cta-block__item cta-block__item--financial-relief">
-        <h3>Financial<br/>Relief</h3>
+        <Fade>
+          <h3>Financial<br/>Relief</h3>
+        </Fade>
       </div>
     </div>
   )

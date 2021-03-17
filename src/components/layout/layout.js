e8e9c9b0d28c0973c6d72f683a32c02aef3d1b47
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-03-17 09:33:23 -0500

    Preload images

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -12,6 +12,9 @@ const Layout = props => {
       <Helmet>
         <link href="//cloud.typenetwork.com/projects/5316/fontface.css/" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="//use.typekit.net/mpp0kpf.css"/>
+        <link rel="preload" as="image" href="/static/nav-bcg-customer-convenience-ace152e7286337049ba218febd96f9e8.jpg"/>
+        <link rel="preload" as="image" href="/static/hero-bcg-health-wellbeing-new-d7e8e1db46bde7f9440304a3dbd1da2d.jpg"/>
+        <link rel="preload" as="image" href="/static/nav-bcg-financial-relief-6961e8e08ea7074f22a87e92ba17e868.jpg"/>
       </Helmet>
       <Header />
       <MegaMenu />
